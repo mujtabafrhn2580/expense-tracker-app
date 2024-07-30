c9afcc0028ee74298585791e98a8616d377b55ba
mujtabafrhn2580 <159253233+mujtabafrhn2580@users.noreply.github.com>
2024-07-30 17:26:10 +0500

    Delete lib directory

--- lib/extensions/round.dart
+++
@@ -1,9 +0,0 @@
-extension NumExtension on num {
-  dynamic roundPercentage() {
-    if (this % 1 == 0) {
-      return this.toInt();
-    } else {
-      return double.parse(this.toStringAsFixed(1));
-    }
-  }
-}
